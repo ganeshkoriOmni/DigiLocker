b606fa15fc0e405535e93c91c3e4d0a14c3add1f
ganeshkori1 <ganesh.kori@omnireach.com>
2024-12-20 13:30:57 +0530

    Login form

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 
-let host = "localhost";
-let port = 2016;
+const host = "localhost";
+const port = 2016;
 const cors = require('cors');
-let express = require("express");
-let url = require('url');
-var mysql      = require('mysql');
+const express = require("express");
+const url = require('url');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 
 let app = express();
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
 
 
@@ -17,27 +22,18 @@ var connection = mysql.createConnection({
   password : 'root',
   database : 'digi_locker'
 });
- 
 
- 
-
-
-let success = {
+var success = {
 	data : '',
 	status : 'Success',
 	code : '200'
 }
 
-let error = {
+var error = {
 	data : '',
-	status : 'Success',
+	status : 'Error',
 	code : '304'
 }
-
-const getQueryData = (request) => {
-	
-}
-
 
 app.use(cors());
 
@@ -47,12 +43,49 @@ app.get("/", function(request, response){
 
 app.post("/login", function(request, response){ //Login Form
 	
+	/*let urlQuery = url.parse(request.url, true);
+	let queryData = urlQuery.query;*/
+
+	console.log("Email : "+request.body.email)
+
+	connection.connect(function(err) {
+	    if (err) {
+			console.log('sql connection error');
+			error.data = err;
+			response.send(error);
+		}
+		
+		connection.query("SELECT * FROM users WHERE email = '"+request.body.email+"' AND password = '"+request.body.password+"'", function (err, result, fields) {
+			if (err) {
+				console.log('Login sql error');
+				error.data = err;
+				response.send(error);
+			}
+			
+			if(result.length >= 1){
+				console.log('Login success');
+				success.data = result;
+				response.send(success);
+			}
+		  });
+	});
+});
+
+app.get("/Register", function(request, response){
+	
 	let urlQuery = url.parse(request.url, true);
 	let queryData = urlQuery.query;
 	
-	success.data = JSON.stringify(queryData);
-	console.log(JSON.stringify(queryData.email));
-    response.send(JSON.stringify(success));
+	connection.connect(function(err) {
+		if (err) throw err;
+		console.log("Connected!");
+		var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+		connection.query(sql, function (err, result) {
+			if (err) throw err;
+			console.log("1 record inserted");
+		});
+	});
+    response.send("Register");
 });
 
 app.get("/dashbord", function(request, response){
