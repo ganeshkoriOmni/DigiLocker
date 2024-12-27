054e6e02cdac1b8108ae6de9e59a13178b6749b4
ganeshkori1 <ganesh.kori@omnireach.com>
2024-12-27 15:00:53 +0530

    Created document add edit option

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post("/CheckEmail", function(request, response){
 
 	console.log(request.body.email)
 
-		connection.query("SELECT * FROM users WHERE email = '"+request.body.email+"'", function (err, result) {
+		connection.query("SELECT * FROM tbl_users WHERE fieldEmail = '"+request.body.email+"'", function (err, result) {
 			if (err) {
 				console.log('sql error');
 				error.data = err;
@@ -70,14 +70,14 @@ app.post("/CheckEmail", function(request, response){
 	
 });
 
-app.post("/login", function(request, response){ //Login Form
+app.post("/Login", function(request, response){ //Login Form
 	
 	/*let urlQuery = url.parse(request.url, true);
 	let queryData = urlQuery.query;*/
 
 	console.log("Email : "+request.body.email)
 		
-		connection.query("SELECT * FROM users WHERE email = '"+request.body.email+"' AND password = '"+request.body.password+"'", function (err, result, fields) {
+		connection.query("SELECT * FROM tbl_users WHERE fieldEmail = '"+request.body.email+"' AND fieldPassword = '"+request.body.password+"'", function (err, result, fields) {
 			if (err) {
 				console.log('Login sql error');
 				error.data = err;
@@ -99,7 +99,7 @@ app.post("/Register", function(request, response){
 	let queryData = urlQuery.query;
 
 		
-		let sqlRegister = "INSERT INTO users (fname, mname, lname, email, password, address) VALUES ('"+request.body.fname+"', '"+request.body.mname+"', '"+request.body.lname+"', '"+request.body.email+"', '"+request.body.password+"', '')";
+		let sqlRegister = "INSERT INTO tbl_users (fieldName, fieldName, fieldEmail, fieldPassword, fieldOption) VALUES ('"+request.body.fname+"', '"+request.body.lname+"', '"+request.body.email+"', '"+request.body.password+"', 'OPEN')";
 		connection.query(sqlRegister, function (err, result) {
 			if (err) {
 				console.log('Register sql error');
@@ -109,6 +109,78 @@ app.post("/Register", function(request, response){
 				console.log('Register success');
 				success.data = result;
 				response.send(success);
+			}
+		  });
+});
+
+app.get("/Documnets", function(request, response){
+	let urlQuery = url.parse(request.url, true);
+	let queryData = urlQuery.query;
+    console.log("Documents : "+queryData.id);
+
+		connection.query("SELECT * FROM tbl_documents WHERE fieldUserId = '"+queryData.id+"'", function (err, result, fields) {
+			if (err) {
+				console.log('Documnets sql error');
+				error.data = err;
+				response.send(err);
+			}else if(result.length >= 1){
+				console.log('Documnets success');
+				success.data = result;
+				response.send(result);
+			}else{
+				response.send(error);
+			}
+		  });
+});
+
+app.get("/DocumnetId", function(request, response){
+	let urlQuery = url.parse(request.url, true);
+	let queryData = urlQuery.query;
+    console.log("Documents ID : "+queryData.id);
+
+		connection.query("SELECT * FROM tbl_documents WHERE fieldId = '"+queryData.id+"'", function (err, result, fields) {
+			if (err) {
+				console.log('DocumnetId sql error');
+				error.data = err;
+				response.send(err);
+			}else if(result.length >= 1){
+				console.log('DocumnetId success');
+				success.data = result;
+				response.send(result);
+			}else{
+				response.send(error);
+			}
+		  });
+});
+
+app.put("/DocumnetUpdate", function(request, response){
+		console.log('request ',request.body.documentsName)
+		let sqlDocument = "UPDATE tbl_documents SET fieldName = '"+request.body.documentsName+"', fieldData = '"+request.body.documentDataGet+"', fieldLastDate = '2024-12-23 23:12:12' WHERE fieldId = '"+request.body.documentsId+"'";
+		connection.query(sqlDocument, function (err, result) {
+			if (err) {
+				console.log('DocumnetUpdate sql error');
+				error.data = err;
+				response.send(error);
+			}else {
+				console.log('DocumnetUpdate success');
+				success.data = result;
+				response.send(success);
+			}
+		  });
+});
+
+app.post("/DocumnetAdd", function(request, response){
+		console.log('request ',request.body.documentsName)
+		let sqlRegister = "INSERT INTO tbl_documents (fieldName, fieldUserId, fieldData, fieldDate) VALUES ('"+request.body.documentsName+"', '"+request.body.userId+"', '"+request.body.documentDataGet+"', '2024-12-23 23:12:12')";
+		connection.query(sqlRegister, function (err, result) {
+			if (err) {
+				console.log('DocumnetAdd sql error');
+				error.data = err;
+				response.send(error);
+			}else {
+				console.log('DocumnetAdd success');
+				success.data = result;
+				response.send(result);
 			}
 		  });
 });
